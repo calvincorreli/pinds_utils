037756b92efaddc184066c3f429b60f5de653347
Lars Pind <lars@pinds.com>
2008-12-06 11:25:28 +0100

    Added my 'focus' view helper method for focusing form elements.

--- lib/pinds_utils/view_helpers.rb
+++ lib/pinds_utils/view_helpers.rb
@@ -3,6 +3,14 @@ module PindsUtils
     def onload(javascript)
       javascript_tag "Event.observe(window, 'load', function() { #{javascript} })"
     end
+
+    def focus(options = {})
+      if options[:id]
+        javascript_tag("document.observe('dom:loaded', function() { $('#{options[:id]}').focus(); })")
+      elsif options[:form]
+        javascript_tag("Form.focusFirstElement('#{options[:form]}')")
+      end
+    end
     
     def open_tag(name, options)
       tag(name, options, true)
